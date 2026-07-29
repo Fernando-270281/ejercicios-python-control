65254f39e37790d7a2de2403ec28a5934bd0d271
Fernando <felixflod27@gmail.com>
2026-07-29 15:38:38 -0600

    feat: Ejercicios de python con if_elif y match-case

---
+++ estacion_año.py
@@ -0,0 +1,15 @@
+# Programa para determinar la estacion del año segun el mes
+mes = input("Ingresa el mes del año: ").strip().lower()
+
+match mes:
+    case "diciembre" | "enero" | "febrero":
+        print("La estación es: Invierno")
+    case "marzo" | "abril" | "mayo":
+        print("La estación es: Primavera")
+    case "junio" | "julio" | "agosto":
+        print("La estación es: Verano")
+    case "septiembre" | "octubre" | "noviembre":
+        print("La estación es: Otoño")
+    case _:
+        print("Mes inválido")
+        
