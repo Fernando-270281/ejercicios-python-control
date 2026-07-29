21904bc7b519a02499712e8be3ea892aa2d4a424
Fernando <felixflod27@gmail.com>
2026-07-29 15:07:14 -0600

    feat: Ejercicios de Oython con if-elif y match-case

---
+++ edad_para_votar.py
@@ -0,0 +1,8 @@
+#Programa para determinar una persona si es mayor de edad para votar 
+edad = int(input("Ingresa tu edad: "))
+
+if edad >= 18:
+    print("Eres mayor de edad. ¡Puedes votar!")
+else:
+    print("Eres menor de edad. No puedes votar aún.")
+    
